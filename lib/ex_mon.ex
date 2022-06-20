986e5a486fdb7e078563a88f30ac9967953a0eb9
thales-pinner <thales.neves@pinpeople.com.br>
2022-06-19 22:58:41 -0300

    Add struct to create player

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,18 +1,3 @@
 defmodule ExMon do
-  @moduledoc """
-  Documentation for `ExMon`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExMon.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
