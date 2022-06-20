c2b6537657fac95bd1136a929c74a51e29e5a5dd
thales-pinner <thales.neves@pinpeople.com.br>
2022-06-19 23:10:07 -0300

    Install credo lib for linters

--- lib/player.ex
+++ lib/player.ex
@@ -6,7 +6,7 @@ defmodule ExMon.Player do
   defstruct @required_keys
 
   def build(name, move_rnd, move_avg, move_heal) do
-    %ExMon.Player{
+    %ExMon.Player {
       life: @max_life,
       move_avg: move_avg,
       move_heal: move_heal,
