fe8ef40f93628e7af5de60ce59be093dc513cda2
thales-pinner <thales.neves@pinpeople.com.br>
2022-06-19 23:06:11 -0300

    Add create player method to call struct

--- lib/player.ex
+++ lib/player.ex
@@ -1,15 +1,17 @@
 defmodule ExMon.Player do
-  @enforce_keys  [:life, :name, :move_avg, :move_rnd, :move_heal]
+  @required_keys [:life, :move_avg, :move_heal, :move_rnd, :name]
+  @max_life 100
+  @enforce_keys  @required_keys
 
-  defstruct [:life, :name, :move_avg, :move_rnd, :move_heal]
+  defstruct @required_keys
 
   def build(name, move_rnd, move_avg, move_heal) do
     %ExMon.Player{
-      name: name,
+      life: @max_life,
       move_avg: move_avg,
-      move_rnd: move_rnd,
       move_heal: move_heal,
-      life: 100
+      move_rnd: move_rnd,
+      name: name
     }
   end
 end
