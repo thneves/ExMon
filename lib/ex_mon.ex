c0a62c994084882c2671857c10311acb2df3c96f
thales-pinner <thales.neves@pinpeople.com.br>
2022-06-21 22:06:45 -0300

    Create Game module and and start game method

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,13 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{ Game, Player }
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
+  end
+
+  def start_game(player) do
+    "Robotinik"
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
   end
 end
