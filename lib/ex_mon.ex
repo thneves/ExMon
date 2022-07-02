d10ca25694d395320ac1b3bb122f6e8ac0f7c24c
thales-pinner <thales.neves@pinpeople.com.br>
2022-07-02 19:27:55 -0300

    Fix game over error

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -18,6 +18,14 @@ defmodule ExMon do
   end
 
   def make_move(move) do
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
+  end
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+
+  defp handle_status(_other, move) do
     move
     |> Actions.fetch_move()
     |> do_move()
@@ -39,4 +47,6 @@ defmodule ExMon do
     move = {:ok, Enum.random(@computer_moves)}
     do_move(move)
   end
+
+  defp computer_move(_), do: :ok
 end
