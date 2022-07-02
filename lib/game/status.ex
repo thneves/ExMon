5a2c875811f88921b1663c5b62826d27f6d0471a
thales-pinner <thales.neves@pinpeople.com.br>
2022-07-02 18:31:49 -0300

    Add player turn logic and game over

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -10,4 +10,12 @@ defmodule ExMon.Game.Status do
   def print_wrong_move_message(move) do
     IO.puts("========== #{move} is a invalid Move ========== \n")
   end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("========== Player attacked Computer dealing #{damage} damage ========== \n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("========== Computer attacked Player dealing #{damage} damage ========== \n")
+  end
 end
